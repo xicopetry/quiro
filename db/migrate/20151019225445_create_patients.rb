8741ea22abb39385510a4832ef073fe8d6432127
Prelang Builder <builder@prelang.com>
2015-10-19 22:54:45 +0000

    Generated model Patient with migration

---
+++ db/migrate/20151019225445_create_patients.rb
@@ -0,0 +1,12 @@
+class CreatePatients < ActiveRecord::Migration
+  def change
+    create_table :patients do |t|
+      t.text :name
+      t.string :phone
+      t.date :birthday
+      t.string :cep
+
+      t.timestamps
+    end
+  end
+end
